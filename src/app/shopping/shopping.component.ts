2a3aa30a0a61041c082ce9b357e9499945984afa
Shrujan Shetty <shrujan.shetty1@ibm.com>
2020-05-29 17:17:57 +0530

    added shopping list and edit

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../../shared/ingredient.model'
+
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
   styleUrls: ['./shopping.component.sass']
 })
 export class ShoppingComponent implements OnInit {
-
+  ingredients:Ingredient[] = [
+    new Ingredient('apples', 6),
+    new Ingredient('tomato', 20)
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+
+    // }
+
+    // TS shortcut for the above code does the same thing
+    constructor(public name:string, public amount: number) {}
+}
