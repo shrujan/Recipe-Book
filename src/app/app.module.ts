faa5aa13193e5cd08dbafcfb54a60920063d4a08
shrujan shetty <shrork@gmail.com>
2020-05-29 11:58:43 +0530

    base code

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { RecipeComponent } from './recipe/recipe.component';
+// import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    RecipeComponent,
+    // ShoppingListComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
